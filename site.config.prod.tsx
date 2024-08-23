0839ec506d4543056373d6086694c123e8b6a7d6
David Joy <davjoy@gmail.com>
2024-08-23 14:19:54 -0400

    feat: getting the project working with frontend-base and a set of complex dependencies
    The project can now work with:
    
    - apps that are imported as dependencies to the site config
    - apps that are deployed as modules straight from their frontend-app repo
    - apps that are deployed as modules from another project.
    
    It does not yet meaningfully handle an “external” app, but this is an important use case.

---
+++ site.config.prod.tsx
@@ -0,0 +1,56 @@
+import { SiteConfig } from '@openedx/frontend-base';
+import './project.scss';
+
+import { ModuleTwo } from '@openedx/frontend-app-base-test';
+
+const config: SiteConfig = {
+  apps: [
+    {
+      appId: 'baseTest',
+      remoteUrl: 'http://localhost:8082/remoteEntry.js',
+      moduleId: 'ModuleOne',
+      path: 'one',
+    },
+    {
+      appId: 'moduleTwo',
+      component: ModuleTwo,
+      path: 'two',
+    },
+    {
+      appId: 'baseTestProject',
+      remoteUrl: 'http://localhost:8083/remoteEntry.js',
+      moduleId: 'ModuleThree',
+      path: 'three',
+    },
+    {
+      appId: 'moduleFour',
+      url: 'http://localhost:8084/four',
+    }
+  ],
+
+  APP_ID: 'base-test',
+  BASE_URL: 'http://localhost:8080',
+  ENVIRONMENT: 'dev',
+  SITE_NAME: 'edX',
+
+  ACCOUNT_PROFILE_URL: 'http://localhost:1995',
+  ACCOUNT_SETTINGS_URL: 'http://localhost:1997',
+  LEARNING_BASE_URL: 'http://localhost:2000',
+  LOGIN_URL: 'http://localhost:18000/login',
+  LOGOUT_URL: 'http://localhost:18000/logout',
+  MARKETING_SITE_BASE_URL: 'http://localhost:18000',
+
+  CREDENTIALS_BASE_URL: 'http://localhost:18150',
+  DISCOVERY_API_BASE_URL: 'http://localhost:18381',
+  ECOMMERCE_BASE_URL: 'http://localhost:18130',
+  LMS_BASE_URL: 'http://localhost:18000',
+  PUBLISHER_BASE_URL: 'http://localhost:18400',
+  STUDIO_BASE_URL: 'http://localhost:18010',
+
+  FAVICON_URL: 'https://edx-cdn.org/v3/default/favicon.ico',
+  LOGO_TRADEMARK_URL: 'https://edx-cdn.org/v3/default/logo-trademark.svg',
+  LOGO_URL: 'https://edx-cdn.org/v3/default/logo.svg',
+  LOGO_WHITE_URL: 'https://edx-cdn.org/v3/default/logo-white.svg',
+};
+
+export default config;
